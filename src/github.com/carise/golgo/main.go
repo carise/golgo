9f8d661fad023cbaa3be48cf7273461da4ef4a0b
Carise Fernandez <carise@gmail.com>
2017-03-12 15:02:08 -0700

    skeleton

--- src/github.com/carise/golgo/main.go
+++ src/github.com/carise/golgo/main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+/**
+ * Game of Life:
+ *
+ * 1. LIVE curr_cell: < 2 live neighbors: die
+ * 2. LIVE curr_cell: 1 < x <= 3 live neighbors: live
+ * 3. LIVE curr_cell: > 3 live neighbors: die
+ * 4. DEAD curr_cell: == 3 live neighbors: live
+ */
+
 func main() {
-    fmt.Printf("Hello, world.\n")
+    Tick()
+}
+
+type Cell struct {
+    Alive bool
+}
+
+func Tick() {
+    PrintCells()
+}
+
+func PrintCells() {
+    fmt.Printf("Print cells:\n")
+}
+
+func (currCell Cell) CountLiveNeighbors() int {
+    return 0
 }
