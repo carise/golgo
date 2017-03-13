ad4c26d5e10bf89febf3522a9ab00dca28f915c7
Carise Fernandez <carise@gmail.com>
2017-03-12 22:38:05 -0700

    More changes

--- src/github.com/carise/golgo/main.go
+++ src/github.com/carise/golgo/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
     "fmt"
+    "math/rand"
+    "time"
 )
 
 /**
- * Game of Life:
+ * Conway's Game of Life:
  *
  * 1. LIVE curr_cell: < 2 live neighbors: die
  * 2. LIVE curr_cell: 1 < x <= 3 live neighbors: live
@@ -14,19 +16,32 @@ import (
  */
 
 type Cell struct {
-    Alive bool
+    CurrAlive bool
+    NextAlive bool
 }
 
 type Grid struct {
+    Size int
     Cells [][]Cell
 }
 
 func main() {
-    row := 10
-    col := 10
+    row := 20
+    col := 30
+    seed := -1 
+    
+    if seed > -1 {
+        rand.Seed(int64(seed))
+    } else {
+        rand.Seed(time.Now().UnixNano())
+    }
+
     grid := &Grid{}
     grid.Init(row, col)
-    grid.Tick()
+
+    for grid.Tick() {
+        time.Sleep(time.Second)
+    }
 }
 
 func (grid *Grid) Init(row int, col int) {
@@ -34,31 +49,90 @@ func (grid *Grid) Init(row int, col int) {
     for i := range cells {
         cells[i] = make([]Cell, col)
     }
+    grid.Size = row * col
     grid.Cells = cells
+    grid.Seed()
 }
 
-func (grid Grid) Tick() {
+func (grid *Grid) Seed() {
+    pct_alive := int(float32(grid.Size) * 0.30)
+    for i := range grid.Cells {
+        row := grid.Cells[i]
+        for j := range row {
+            r := rand.Intn(grid.Size)
+            row[j].CurrAlive, row[j].NextAlive = r < pct_alive, r < pct_alive
+        }
+    }
     grid.PrintGrid()
 }
 
-func (grid Grid) PrintGrid() {
-    fmt.Printf("Print cells:\n")
-    for i := 0; i < len(grid.Cells); i++ {
+func (grid *Grid) PrintGrid() {
+    fmt.Println("----------------------------")
+    for i := range grid.Cells {
         row := grid.Cells[i]
-        for j := 0; j < len(row); j++ {
-            if row[j].Alive {
+        for j := range row {
+            if row[j].NextAlive {
                 fmt.Printf(" x ")
             } else {
                 fmt.Printf(" . ")
             }
+            row[j].CurrAlive = row[j].NextAlive
         }
         fmt.Printf("\n")
     }
 }
 
-func (grid Grid) SeedGrid() {
+func (grid *Grid) Tick() bool {
+    changed := false
+    for i := range grid.Cells {
+        row := grid.Cells[i]
+        for j := range row {
+            count := grid.CountLiveNeighbors(i, j)
+            if row[j].CurrAlive {
+                row[j].NextAlive = count == 2 || count == 3
+            } else {
+                row[j].NextAlive = count == 3
+            }
+            changed = changed || row[j].CurrAlive != row[j].NextAlive
+        }
+    }
+    grid.PrintGrid()
+    return changed
 }
 
-func (currCell Cell) CountLiveNeighbors() int {
-    return 0
+func (grid Grid) CountLiveNeighbors(row int, col int) int {
+    x_l := col - 1
+    x_r := col + 1
+    y_t := row - 1
+    y_b := row + 1
+
+    if x_l < 0 {
+        x_l = 0
+    }
+
+    if x_r >= len(grid.Cells[row]) {
+        x_r = len(grid.Cells[row]) - 1
+    }
+
+    if y_t < 0 {
+        y_t = 0
+    }
+
+    if y_b >= len(grid.Cells) {
+        y_b = len(grid.Cells) - 1
+    }
+
+    count := 0
+
+    for i := y_t; i <= y_b; i++ {
+        for j := x_l; j <= x_r; j++ {
+            if i == row && j == col {
+                continue
+            }
+            if grid.Cells[i][j].CurrAlive {
+                count++
+            }
+        }
+    }
+    return count
 }
